6bb66ba610b0a95f593031c13de1b0e4ef041470
Lucas Schlierf <lucas.schlierf@tum.de>
2024-03-08 00:00:36 +0100

    added legal disclaimers

--- app/(legals)/impressum/page.tsx
+++ app/(legals)/impressum/page.tsx
@@ -1,27 +1,35 @@
 export default function Impressum() {
   return (
     <>
-      <u>Impressum</u>
-      <br />
-      <br />
-      Angaben gemäß § 5 TMG:
-      <br />
-      <br />
-      Sevenheaven GbR
+      <h1>Impressum</h1>
+      <h2 id="m46">Diensteanbieter</h2>
+      Seven Heaven GbR
       <br />
       Lucas Schlierf
       <br />
       Klingerstraße 31
       <br />
-      81369 München
-      <br/>
-      admin@sevenheaven.band
+      81369
+
+      <h2 id="m56">Kontaktmöglichkeiten</h2>
+      E-Mail-Adresse: admin@sevenheaven
       <br />
-      <br />
-      Bei Fragen oder Hinweisen zum Inhalt wenden Sie sich bitte an kontakt@sevenheaven.band oder melden Sie sich unter der Telefonnummer 089/71034962.
-      <br />
-      <br />
-      Website designt von Julian Höflmaier, gebaut von Lucas Schlierf.
+      Telefon: 089/71034962
+
+      <h2 id="m153">Vertretungsberechtigte Personen</h2>Vertretungsberechtigt: Lucas Schlierf
+
+      <h2 id="m3589">Audiovisuelle Mediendienste</h2><p>Sitzland: Deutschland.</p>
+      <p>Zuständige Regulierungs-, bzw. Aufsichtsbehörde: Bayerische Landeszentrale für neue Medien (BLM), <a href="https://www.blm.de/" target="_blank">https://www.blm.de/</a></p>
+
+      <h2 id="m172">Social Media und andere Onlinepräsenzen</h2><p>Dieses Impressum gilt auch für die folgenden Social-Media-Präsenzen und Onlineprofile: </p>
+      <p><a href="https://instagram.com/sevenheaven.band" target="_blank">https://instagram.com/sevenheaven.band</a></p>
+      <p><a href="https://www.youtube.com/@7HeavenBand" target="_blank">https://www.youtube.com/@7HeavenBand</a></p>
+
+      <h2 id="m65">Haftungs- und Schutzrechtshinweise</h2><p>Urheberrechte und Markenrechte: Alle auf dieser Website dargestellten Inhalte, wie Texte, Fotografien, Grafiken, Marken und Warenzeichen sind durch die jeweiligen Schutzrechte (Urheberrechte, Markenrechte) geschützt. Die Verwendung, Vervielfältigung usw. unterliegen unseren Rechten oder den Rechten der jeweiligen Urheber bzw. Rechteinhaber.</p>
+
+      <h2>Design und Betrieb</h2><p>Website designt von Julian Höflmaier, gebaut von Lucas Schlierf.</p>
+
+      <p><a href="https://datenschutz-generator.de/" title="Rechtstext von Dr. Schwenke - für weitere Informationen bitte anklicken." target="_blank" rel="noopener noreferrer nofollow">Erstellt mit kostenlosem Datenschutz-Generator.de von Dr. Thomas Schwenke</a></p>
     </>
   )
 }
