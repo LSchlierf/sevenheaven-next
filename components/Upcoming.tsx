8385f650a8a8020d630535eedfb12ad36599e08a
Lucas Schlierf <LucasSchlierf@gmail.com>
2025-06-23 16:01:47 +0200

    removed old poster

--- components/Upcoming.tsx
+++ components/Upcoming.tsx
@@ -5,7 +5,7 @@ import poster_2025_06_09 from '@/public/img/posters/2025-06-09.png'
 
 export default function upcoming() {
   return <>
-    <H2 text='Kommende Gigs' />
+    {/* <H2 text='Kommende Gigs' />
     <Poster href='https://musikalisches-weinfest.de/' external image={poster_2025_06_09} text={<>
       Auch dieses Jahr sind wir wieder beim Musikalischen Weinfest im Westpark vertreten.
       <br />
@@ -14,6 +14,6 @@ export default function upcoming() {
       <br />
       <br />
       Also kommt am <b>Pfingstmontag, den 9. Juni</b> zur Seebühne im Westpark und bringt Durst und eure Liebsten mit, für die Stimmung sorgen wir! 😉
-    </>} />
+    </>} /> */}
   </>
 }
